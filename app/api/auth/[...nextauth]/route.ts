551f1bb5bf3439e2ce13efb90e575b5ca0cabe5a
Color-Kat <katik7979@gmail.com>
2023-11-17 21:43:10 +0300

    feat(auth): env, server side auth, next-auth types, user model

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,18 +1,48 @@
 import GoogleProvider from 'next-auth/providers/google';
 import NextAuth, {SessionOptions} from "next-auth";
+import {connectToDB} from "@/utilsdatabase";
+import User from "@models/user";
 
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: ''
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
-    async session({session}: any) {
 
-    },
-    async signIn({profile}: any) {
+    callbacks: {
+        async session({session}) {
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            });
 
+            session.user.id = sessionUser._id.toString();
+
+            return session;
+        },
+
+        async signIn({profile}: any) {
+            try {
+                await connectToDB();
+
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+
+                if (!userExists)
+                    await User.create({
+                        email: profile.email,
+                        name: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.image
+                    })
+
+                return true;
+            } catch (error) {
+                console.log(error);
+                return false
+            }
+        }
     }
 });
 
