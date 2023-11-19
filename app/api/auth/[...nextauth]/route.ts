dff6d168af902a840ede467ecb46b5ae94ee9917
Color-Kat <katik7979@gmail.com>
2023-11-19 12:58:05 +0300

    providers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,19 @@
 import GoogleProvider from 'next-auth/providers/google';
-import NextAuth from "next-auth";
+import NextAuth, {SessionOptions} from "next-auth";
 
-// const handler = NextAuth({
-//
-// });
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: '',
+            clientSecret: ''
+        })
+    ],
+    async session({session}: any) {
+
+    },
+    async signIn({profile}: any) {
+
+    }
+});
+
+export {handler as GET, handler as POST}
